a2a7930f5433bd63d8975b9c500a5ca4002ea786
khoasv89 <111332789+khoasv89@users.noreply.github.com>
2023-09-25 15:24:32 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,47 +1,37 @@
 // Create web server
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
 const app = express();
-const port = 3000;
-// Create database connection
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/CommentDB', { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-// Create schema
-const commentSchema = new mongoose.Schema({
-    name: String,
-    comment: String
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Create event handler
+app.post('/events', async (req, res) => {
+  // Get data from event
+  const { type, data } = req.body;
+
+  // Check event type
+  if (type === 'CommentCreated') {
+    // If comment contains 'orange' then set status to 'rejected'
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+    // Send event to event bus
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        ...data,
+        status,
+      },
+    });
+  }
+
+  // Send response
+  res.send({});
 });
-// Create model
-const Comment = mongoose.model('Comment', commentSchema);
-// Create sample comments
-const comment1 = new Comment({ name: 'John', comment: 'Hello World' });
-const comment2 = new Comment({ name: 'Mary', comment: 'Hi, nice to meet you' });
-// Save sample comments
-comment1.save(function (err, comment) {
-    if (err) return console.error(err);
-    console.log(comment.name + " saved.");
-});
-comment2.save(function (err, comment) {
-    if (err) return console.error(err);
-    console.log(comment.name + " saved.");
-});
-// Set view engine
-app.set('view engine', 'pug');
-// Set static files
-app.use(express.static('public'));
-// Set routes
-app.get('/', function (req, res) {
-    res.render('index');
-});
-app.get('/comments', function (req, res) {
-    Comment.find(function (err, comments) {
-        if (err) return console.error(err);
-        console.log(comments);
-        res.render('comments', { comments: comments });
-    })
-});
+
 // Start server
-app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+app.listen(4003, () => {
+  console.log('Listening on 4003');
 });
